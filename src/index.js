ed03b35d761cdceebbcc5ae1c23dcf61f80ef495
DionataRech <dionata.rech@gmail.com>
2024-03-25 17:33:02 -0300

    interacoes ,Correcoes de bug do setimeout e conteudos para o readme

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// setTimeout(() => {
-//   window.location.href = "./home.html";
-// }, 10500);
-
-function Trailer() {
-  document.getElementById("trailer").style.display = "block";
-}
+setTimeout(() => {
+  window.location.href = "./home.html";
+}, 10500);
