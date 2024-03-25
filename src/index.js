4d65f0983dcab97f81feb0fa457bb8829a2913d3
DionataRech <dionata.rech@gmail.com>
2024-03-25 15:30:43 -0300

    refatorando

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-setTimeout(() => {
-  window.location.href = "./home.html";
-}, 10500);
+// setTimeout(() => {
+//   window.location.href = "./home.html";
+// }, 10500);
 
 function Trailer() {
   document.getElementById("trailer").style.display = "block";
